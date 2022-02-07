5ca4f175547d97ddbcf8caed0851f29c52294631
HP-PC <amira28005@gmail.com>
2022-02-07 21:56:07 +0300

    change student code

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import telebot
-import sqlite3
+#import telebot
+#import sqlite3
 
-bot = telebot.TeleBot('5209932574:AAFJpNe9bt1fhMma-jwGNP5YSoGJqWjcrh8')
+#bot = telebot.TeleBot('5209932574:AAFJpNe9bt1fhMma-jwGNP5YSoGJqWjcrh8')
 
+import qrcode
+data = "Салоп Максим/7а/05.02.22/2334"
+filename = "qrcode14"
+img = qrcode.make(data)
+img.save(filename)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    connect = sqlite3.connect('users.db')
-    cursor = connect.cursor()
-    
-    cursor.execute("""CREATE TABLE IF NOT EXISTS login_id()""")
-
-
-@bot.message_handler(commands=['delete'])
-def delete(message):
-    pass
